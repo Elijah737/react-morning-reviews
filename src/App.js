a8f08a5d451b64794cb7de914d808b3317025297
Elijah Brown <elijahbrown737@gmail.com>
2020-07-22 09:33:12 -0700

    react day2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+// import React, { Component } from 'react'
 
+// class App extends Component {
+//     constructor(){
+//         super()
+//         this.state = {
+
+//         }
+//     }
+//     render(){
+//         return <h1> This is THE App Component </h1>
+//     }
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+import './App.css'
+import List from './Components/List'
+
+const App = () => {
+    return (
+        <div className='App' >
+            <h1 className='movie-list-title' > The Most Amazing Movie List </h1>
+            <List />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/List.js
@@ -0,0 +1,18 @@
+// const { Component } = require("react")
+
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor(){
+        super()
+        
+        this.state = {
+            movies: []
+        }
+    }
+    render(){
+        return <h1> This is The List Component </h1>
+    }
+}
+
+export default List
